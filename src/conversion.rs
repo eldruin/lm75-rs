93b4234216023cd9e4f0dd31b983205acde77d7e
dkhayes117 <dkhayes117@yahoo.com>
2021-08-04 19:15:45 -0400

    Move resolution methods to markers.rs file

--- src/conversion.rs
+++ src/conversion.rs
@@ -1,14 +1,13 @@
 //! Value conversions
-use crate::Resolution;
 
-pub fn convert_temp_from_register(msb: u8, lsb: u8, mask: Resolution) -> f32 {
+pub fn convert_temp_from_register(msb: u8, lsb: u8) -> f32 {
     // msb is stored as two's complement
     let msb = msb as i8 as f32;
     let decimal = ((lsb & mask as u8) >> 5) as f32 * 0.125;
     msb + decimal
 }
 
-pub fn convert_temp_to_register(temp: f32, mask: Resolution) -> (u8, u8) {
+pub fn convert_temp_to_register(temp: f32) -> (u8, u8) {
     let int = (temp / 0.125) as i16 as u16;
     let binary = int << 5;
     let msb = (binary >> 8) as u8;
@@ -23,69 +22,69 @@ mod tests {
 
     #[test]
     fn can_convert_temperature_from_register() {
-        assert_eq!(convert_temp_from_register(0b0111_1101, 0b0101_1010, Mask9bit), 125.0);
-        assert_eq!(convert_temp_from_register(0b0001_1001, 0b0101_1010, Mask9bit), 25.0);
-        assert_eq!(convert_temp_from_register(0b1110_0111, 0b0101_1010, Mask9bit), -25.0);
-        assert_eq!(convert_temp_from_register(0b1100_1001, 0b0101_1010, Mask9bit), -55.0);
-        assert_eq!(convert_temp_from_register(0b0000_0000, 0b0101_1010, Mask9bit), 0.0);
-        assert_eq!(convert_temp_from_register(0b0000_0000, 0b1101_1010, Mask9bit), 0.5);
-        assert_eq!(convert_temp_from_register(0b0010_0000, 0b0101_1010, Mask9bit), 32.0);
-        assert_eq!(convert_temp_from_register(0b0100_1011, 0b0101_1010, Mask9bit), 75.0);
-        assert_eq!(convert_temp_from_register(0b0101_0000, 0b0101_1010, Mask9bit), 80.0);
-        assert_eq!(convert_temp_from_register(0b0111_1111, 0b1101_1010, Mask9bit), 127.5);
-        assert_eq!(convert_temp_from_register(0b1111_1111, 0b1101_1010, Mask9bit), -0.5);
-        assert_eq!(convert_temp_from_register(0b1111_1111, 0b0101_1010, Mask9bit), -1.0);
-        assert_eq!(convert_temp_from_register(0b1111_1101, 0b1101_1010, Mask9bit), -2.5);
-        assert_eq!(convert_temp_from_register(0b1110_0000, 0b0101_1010, Mask9bit), -32.0);
-        assert_eq!(convert_temp_from_register(0b1000_0000, 0b1101_1010, Mask9bit), -127.5);
-        assert_eq!(convert_temp_from_register(0b1000_0000, 0b0101_1010, Mask9bit), -128.0);
+        assert_eq!(convert_temp_from_register(0b0111_1101, 0b0101_1010), 125.0);
+        assert_eq!(convert_temp_from_register(0b0001_1001, 0b0101_1010), 25.0);
+        assert_eq!(convert_temp_from_register(0b1110_0111, 0b0101_1010), -25.0);
+        assert_eq!(convert_temp_from_register(0b1100_1001, 0b0101_1010), -55.0);
+        assert_eq!(convert_temp_from_register(0b0000_0000, 0b0101_1010), 0.0);
+        assert_eq!(convert_temp_from_register(0b0000_0000, 0b1101_1010), 0.5);
+        assert_eq!(convert_temp_from_register(0b0010_0000, 0b0101_1010), 32.0);
+        assert_eq!(convert_temp_from_register(0b0100_1011, 0b0101_1010), 75.0);
+        assert_eq!(convert_temp_from_register(0b0101_0000, 0b0101_1010), 80.0);
+        assert_eq!(convert_temp_from_register(0b0111_1111, 0b1101_1010), 127.5);
+        assert_eq!(convert_temp_from_register(0b1111_1111, 0b1101_1010), -0.5);
+        assert_eq!(convert_temp_from_register(0b1111_1111, 0b0101_1010), -1.0);
+        assert_eq!(convert_temp_from_register(0b1111_1101, 0b1101_1010), -2.5);
+        assert_eq!(convert_temp_from_register(0b1110_0000, 0b0101_1010), -32.0);
+        assert_eq!(convert_temp_from_register(0b1000_0000, 0b1101_1010), -127.5);
+        assert_eq!(convert_temp_from_register(0b1000_0000, 0b0101_1010), -128.0);
 
-        assert_eq!(convert_temp_from_register(0b0111_1101, 0b0101_1010, Mask11bit), 125.250);
-        assert_eq!(convert_temp_from_register(0b0001_1001, 0b0101_1010, Mask11bit), 25.250);
-        assert_eq!(convert_temp_from_register(0b1110_0111, 0b0101_1010, Mask11bit), -24.750);
-        assert_eq!(convert_temp_from_register(0b1100_1001, 0b0101_1010, Mask11bit), -54.750);
-        assert_eq!(convert_temp_from_register(0b0000_0000, 0b0101_1010, Mask11bit), 0.250);
-        assert_eq!(convert_temp_from_register(0b0000_0000, 0b1101_1010, Mask11bit), 0.750);
-        assert_eq!(convert_temp_from_register(0b0010_0000, 0b0101_1010, Mask11bit), 32.250);
-        assert_eq!(convert_temp_from_register(0b0100_1011, 0b0101_1010, Mask11bit), 75.250);
-        assert_eq!(convert_temp_from_register(0b0101_0000, 0b0101_1010, Mask11bit), 80.250);
-        assert_eq!(convert_temp_from_register(0b0111_1111, 0b1101_1010, Mask11bit), 127.750);
-        assert_eq!(convert_temp_from_register(0b1111_1111, 0b1101_1010, Mask11bit), -0.250);
-        assert_eq!(convert_temp_from_register(0b1111_1111, 0b0101_1010, Mask11bit), -0.750);
-        assert_eq!(convert_temp_from_register(0b1111_1101, 0b1101_1010, Mask11bit), -2.250);
-        assert_eq!(convert_temp_from_register(0b1110_0000, 0b0101_1010, Mask11bit), -31.750);
-        assert_eq!(convert_temp_from_register(0b1000_0000, 0b1101_1010, Mask11bit), -127.250);
-        assert_eq!(convert_temp_from_register(0b1000_0000, 0b0101_1010, Mask11bit), -127.750);
+        assert_eq!(convert_temp_from_register(0b0111_1101, 0b0101_1010), 125.250);
+        assert_eq!(convert_temp_from_register(0b0001_1001, 0b0101_1010), 25.250);
+        assert_eq!(convert_temp_from_register(0b1110_0111, 0b0101_1010), -24.750);
+        assert_eq!(convert_temp_from_register(0b1100_1001, 0b0101_1010), -54.750);
+        assert_eq!(convert_temp_from_register(0b0000_0000, 0b0101_1010), 0.250);
+        assert_eq!(convert_temp_from_register(0b0000_0000, 0b1101_1010), 0.750);
+        assert_eq!(convert_temp_from_register(0b0010_0000, 0b0101_1010), 32.250);
+        assert_eq!(convert_temp_from_register(0b0100_1011, 0b0101_1010), 75.250);
+        assert_eq!(convert_temp_from_register(0b0101_0000, 0b0101_1010), 80.250);
+        assert_eq!(convert_temp_from_register(0b0111_1111, 0b1101_1010), 127.750);
+        assert_eq!(convert_temp_from_register(0b1111_1111, 0b1101_1010), -0.250);
+        assert_eq!(convert_temp_from_register(0b1111_1111, 0b0101_1010), -0.750);
+        assert_eq!(convert_temp_from_register(0b1111_1101, 0b1101_1010), -2.250);
+        assert_eq!(convert_temp_from_register(0b1110_0000, 0b0101_1010), -31.750);
+        assert_eq!(convert_temp_from_register(0b1000_0000, 0b1101_1010), -127.250);
+        assert_eq!(convert_temp_from_register(0b1000_0000, 0b0101_1010), -127.750);
     }
 
     #[test]
     fn can_convert_temperature_to_register() {
-        assert_eq!((0b0000_0010, 0b0000_0000), convert_temp_to_register(2.4, Mask9bit));
-        assert_eq!((0b0000_0010, 0b1000_0000), convert_temp_to_register(2.6, Mask9bit));
-        assert_eq!((0b1111_1110, 0b0000_0000), convert_temp_to_register(-2.0, Mask9bit));
-        assert_eq!((0b1111_1101, 0b1000_0000), convert_temp_to_register(-2.6, Mask9bit));
-        assert_eq!((0b0111_1101, 0b0000_0000), convert_temp_to_register(125.0, Mask9bit));
-        assert_eq!((0b0001_1001, 0b0000_0000), convert_temp_to_register(25.0, Mask9bit));
-        assert_eq!((0b1110_0111, 0b0000_0000), convert_temp_to_register(-25.0, Mask9bit));
-        assert_eq!((0b1100_1001, 0b0000_0000), convert_temp_to_register(-55.0, Mask9bit));
-        assert_eq!((0b0000_0000, 0b0000_0000), convert_temp_to_register(0.0, Mask9bit));
-        assert_eq!((0b0000_0000, 0b1000_0000), convert_temp_to_register(0.5, Mask9bit));
-        assert_eq!((0b0010_0000, 0b0000_0000), convert_temp_to_register(32.0, Mask9bit));
-        assert_eq!((0b0111_1111, 0b1000_0000), convert_temp_to_register(127.5, Mask9bit));
-        assert_eq!((0b1000_0000, 0b0000_0000), convert_temp_to_register(-128.0, Mask9bit));
+        assert_eq!((0b0000_0010, 0b0000_0000), convert_temp_to_register(2.4));
+        assert_eq!((0b0000_0010, 0b1000_0000), convert_temp_to_register(2.6));
+        assert_eq!((0b1111_1110, 0b0000_0000), convert_temp_to_register(-2.0));
+        assert_eq!((0b1111_1101, 0b1000_0000), convert_temp_to_register(-2.6));
+        assert_eq!((0b0111_1101, 0b0000_0000), convert_temp_to_register(125.0));
+        assert_eq!((0b0001_1001, 0b0000_0000), convert_temp_to_register(25.0));
+        assert_eq!((0b1110_0111, 0b0000_0000), convert_temp_to_register(-25.0));
+        assert_eq!((0b1100_1001, 0b0000_0000), convert_temp_to_register(-55.0));
+        assert_eq!((0b0000_0000, 0b0000_0000), convert_temp_to_register(0.0));
+        assert_eq!((0b0000_0000, 0b1000_0000), convert_temp_to_register(0.5));
+        assert_eq!((0b0010_0000, 0b0000_0000), convert_temp_to_register(32.0));
+        assert_eq!((0b0111_1111, 0b1000_0000), convert_temp_to_register(127.5));
+        assert_eq!((0b1000_0000, 0b0000_0000), convert_temp_to_register(-128.0));
 
-        assert_eq!((0b0000_0010, 0b0110_0000), convert_temp_to_register(2.4, Mask11bit));
-        assert_eq!((0b0000_0010, 0b1000_0000), convert_temp_to_register(2.6, Mask11bit));
-        assert_eq!((0b1111_1110, 0b0000_0000), convert_temp_to_register(-2.0, Mask11bit));
-        assert_eq!((0b1111_1101, 0b1000_0000), convert_temp_to_register(-2.6, Mask11bit));
-        assert_eq!((0b0111_1101, 0b0000_0000), convert_temp_to_register(125.0, Mask11bit));
-        assert_eq!((0b0001_1001, 0b0000_0000), convert_temp_to_register(25.0, Mask11bit));
-        assert_eq!((0b1110_0111, 0b0000_0000), convert_temp_to_register(-25.0, Mask11bit));
-        assert_eq!((0b1100_1001, 0b0000_0000), convert_temp_to_register(-55.0, Mask11bit));
-        assert_eq!((0b0000_0000, 0b0000_0000), convert_temp_to_register(0.0, Mask11bit));
-        assert_eq!((0b0000_0000, 0b1000_0000), convert_temp_to_register(0.5, Mask11bit));
-        assert_eq!((0b0010_0000, 0b0000_0000), convert_temp_to_register(32.0, Mask11bit));
-        assert_eq!((0b0111_1111, 0b1000_0000), convert_temp_to_register(127.5, Mask11bit));
-        assert_eq!((0b1000_0000, 0b0000_0000), convert_temp_to_register(-128.0, Mask11bit));
+        assert_eq!((0b0000_0010, 0b0110_0000), convert_temp_to_register(2.4));
+        assert_eq!((0b0000_0010, 0b1000_0000), convert_temp_to_register(2.6));
+        assert_eq!((0b1111_1110, 0b0000_0000), convert_temp_to_register(-2.0));
+        assert_eq!((0b1111_1101, 0b1000_0000), convert_temp_to_register(-2.6));
+        assert_eq!((0b0111_1101, 0b0000_0000), convert_temp_to_register(125.0));
+        assert_eq!((0b0001_1001, 0b0000_0000), convert_temp_to_register(25.0));
+        assert_eq!((0b1110_0111, 0b0000_0000), convert_temp_to_register(-25.0));
+        assert_eq!((0b1100_1001, 0b0000_0000), convert_temp_to_register(-55.0));
+        assert_eq!((0b0000_0000, 0b0000_0000), convert_temp_to_register(0.0));
+        assert_eq!((0b0000_0000, 0b1000_0000), convert_temp_to_register(0.5));
+        assert_eq!((0b0010_0000, 0b0000_0000), convert_temp_to_register(32.0));
+        assert_eq!((0b0111_1111, 0b1000_0000), convert_temp_to_register(127.5));
+        assert_eq!((0b1000_0000, 0b0000_0000), convert_temp_to_register(-128.0));
     }
 }
--- src/markers.rs
+++ src/markers.rs
@@ -25,3 +25,34 @@ impl<E> SampleRateSupport<E> for ic::Pct2075 {
         ((byte as u16) & BitFlags::SAMPLE_RATE_MASK) * 100
     }
 }
+
+pub trait ResolutionSupport<E>: private::Sealed {
+    fn check_value_is_appropriate(value: u16) -> Result<(), Error<E>>;
+    fn get_value_for_i2c(value: u16) -> [u8; 2];
+}
+
+impl<E> ResolutionSupport<E> for ic::Pct2075 {
+    fn check_value_is_appropriate(value: u16) -> Result<(), Error<E>> {
+        if value >= 1 << 9 {
+            Err(Error::InvalidInputData)
+        } else {
+            Ok(())
+        }
+    }
+    fn get_value_for_i2c(value: u16) -> [u8; 2] {
+        [(value >> 5) as u8, (value & 0xff) as u8]
+    }
+}
+
+impl<E> ResolutionSupport<E> for ic::Lm75 {
+    fn check_value_is_appropriate(value: u16) -> Result<(), Error<E>> {
+        if value >= 1 << 11 {
+            Err(Error::InvalidInputData)
+        } else {
+            Ok(())
+        }
+    }
+    fn get_value_for_i2c(value: u16) -> [u8; 2] {
+        [(value >> 7) as u8, (value & 0xff) as u8]
+    }
+}
